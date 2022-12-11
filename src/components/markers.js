dc0721e0168f6085b56ee5be9a2b46352e01c713
Benji-Montgomery <benjimon678@yahoo.com>
2022-12-11 12:02:03 -0800

    first commit

---
+++ src/components/markers.js
@@ -0,0 +1,46 @@
+const markers = [
+    {
+        name: 'Bremerton',
+        lat: 47.492257,
+        lng: -122.754693
+    },
+    {
+        name: 'IMI',
+        lat: 40.043932,
+        lng: -104.949296
+    },
+    {
+        name: 'Mac',
+        lat: 45.225621,
+        lng: -123.170771
+    },
+    {
+        name: 'Shasta',
+        lat: 40.504167,
+        lng: -122.285629
+    },
+    {
+        name: 'SKRA',
+        lat: 47.657778,
+        lng: -117.591583
+    },
+    {
+        name: 'Sultan',
+        lat: 47.864050,
+        lng: -121.796850
+    },
+    {
+        name: 'TCKC',
+        lat: 46.349114,
+        lng: -119.347580
+    },
+    {
+        name: 'UMC',
+        lat: 40.584648,
+        lng: -112.377699
+    }
+
+    
+]
+
+export default markers
