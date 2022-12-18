69460b758720676c043f076229687c13028aa9bd
Benji-Montgomery <benjimon678@yahoo.com>
2022-12-18 07:17:35 -0800

    contact updated

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,35 +1,10 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
-
+import './css/contact.css'
 const Contact = () => {
-  const navigate = useNavigate()
-  const addNote = () =>{
-    console.log('hiii')
-    navigate("/")
-  }
+
 return (
 	<div>
-	<form name="contact" onSubmit={addNote}>
-  <p>
-    <label>Your Name: <input type="text" name="name" /></label>
-  </p>
-  <p>
-    <label>Your Email: <input type="email" name="email" /></label>
-  </p>
-  <p>
-    <label>Your Role: <select name="role[]" multiple>
-      <option value="leader">Leader</option>
-      <option value="follower">Follower</option>
-    </select></label>
-  </p>
-  <p>
-    <label>Message: <textarea name="message"></textarea></label>
-  </p>
-  <p>
-  	<button id="submitButton" type="submit" >submit</button>
-  </p>
-</form>
-
+    <iframe src='https://docs.google.com/forms/d/e/1FAIpQLSc-h1HVDSm1FzEyQWziX7xJbpY1YXljkCrcqSDCjFyg747djA/viewform' title='Contact Form'></iframe>
 	</div>
 );
 };
