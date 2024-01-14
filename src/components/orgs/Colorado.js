bf4ceb82de395e6d888805c79b909df34b8d1745
Benji-Montgomery <benjimon678@yahoo.com>
2024-01-14 08:18:16 -0800

    puyallup

--- src/components/orgs/Colorado.js
+++ src/components/orgs/Colorado.js
@@ -1,42 +1,42 @@
 const name = 'Colorado_Supermoto'
 
 const races = [
-    {
-        name: name,
-        track: 'Pueblo Motorsports Park - Pueblo, Co' ,
-        date: [5,14],
-        sortDate: .0514
-    },
-    {
-        name: name,
-        track: 'IMI Motorsports Park - Dacono, Co',
-        date: [6,4],
-        sortDate: .0604
-    },
-    {
-        name: name,
-        track: 'Colorado National Speedway - Dacono, Co',
-        date: [7,9],
-        sortDate: .0709
-    },
-    {
-        name: name,
-        track: 'IMI Motorsports Park - Dacono, Co',
-        date: [7,30],
-        sortDate: .0730
-    },
-    {
-        name: name,
-        track: 'Colorado National Speedway - Dacono, Co',
-        date: [8,20],
-        sortDate: .0813
-    },
-    {
-        name: name,
-        track: 'Grand Junction Motor Speedway - Grand Junction, Co',
-        date: [9,17],
-        sortDate: .0917
-    }
+    // {
+    //     name: name,
+    //     track: 'Pueblo Motorsports Park - Pueblo, Co' ,
+    //     date: [5,14],
+    //     sortDate: .0514
+    // },
+    // {
+    //     name: name,
+    //     track: 'IMI Motorsports Park - Dacono, Co',
+    //     date: [6,4],
+    //     sortDate: .0604
+    // },
+    // {
+    //     name: name,
+    //     track: 'Colorado National Speedway - Dacono, Co',
+    //     date: [7,9],
+    //     sortDate: .0709
+    // },
+    // {
+    //     name: name,
+    //     track: 'IMI Motorsports Park - Dacono, Co',
+    //     date: [7,30],
+    //     sortDate: .0730
+    // },
+    // {
+    //     name: name,
+    //     track: 'Colorado National Speedway - Dacono, Co',
+    //     date: [8,20],
+    //     sortDate: .0813
+    // },
+    // {
+    //     name: name,
+    //     track: 'Grand Junction Motor Speedway - Grand Junction, Co',
+    //     date: [9,17],
+    //     sortDate: .0917
+    // }
 ]
 const exportRaces = {
     races, name
