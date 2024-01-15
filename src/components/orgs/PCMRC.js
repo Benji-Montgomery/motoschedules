7ce89c3aed88c74a6ac9fe520c194608e4a46a22
Benji-Montgomery <benjimon678@yahoo.com>
2024-01-15 14:47:43 -0800

    comment out

--- src/components/orgs/PCMRC.js
+++ src/components/orgs/PCMRC.js
@@ -1,36 +1,36 @@
 const name = 'PCMRC'
 
 const races = [
-    {
-        name: name,
-        track: 'Greg Moore Raceway - British Columbia, CAN' ,
-        date: [5,27,28],
-        sortDate: .0527
-    },
-    {
-        name: name,
-        track: 'Greg Moore Raceway - British Columbia, CAN',
-        date: [6,24,25],
-        sortDate: .0624
-    },
-    {
-        name: name,
-        track: 'Greg Moore Raceway - British Columbia, CAN',
-        date: [7,29,30],
-        sortDate: .0729
-    },
-    {
-        name: name,
-        track: 'Cariboo Raceway Park - Quesnel, CAN',
-        date: [8,5,6],
-        sortDate: .0805
-    },
-    {
-        name: name,
-        track: 'Greg Moore Raceway - British Columbia, CAN',
-        date: [9,9,10],
-        sortDate: .0910
-    }
+    // {
+    //     name: name,
+    //     track: 'Greg Moore Raceway - British Columbia, CAN' ,
+    //     date: [5,27,28],
+    //     sortDate: .0527
+    // },
+    // {
+    //     name: name,
+    //     track: 'Greg Moore Raceway - British Columbia, CAN',
+    //     date: [6,24,25],
+    //     sortDate: .0624
+    // },
+    // {
+    //     name: name,
+    //     track: 'Greg Moore Raceway - British Columbia, CAN',
+    //     date: [7,29,30],
+    //     sortDate: .0729
+    // },
+    // {
+    //     name: name,
+    //     track: 'Cariboo Raceway Park - Quesnel, CAN',
+    //     date: [8,5,6],
+    //     sortDate: .0805
+    // },
+    // {
+    //     name: name,
+    //     track: 'Greg Moore Raceway - British Columbia, CAN',
+    //     date: [9,9,10],
+    //     sortDate: .0910
+    // }
 ]
 const exportRaces = {
     races, name
