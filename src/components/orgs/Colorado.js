cf46a1b466768035160ffdf654af7f75cbf6a201
Benji-Montgomery <benjimon678@yahoo.com>
2024-02-25 14:47:11 -0800

    colorado

--- src/components/orgs/Colorado.js
+++ src/components/orgs/Colorado.js
@@ -1,42 +1,43 @@
 const name = 'Colorado_Supermoto'
 
 const races = [
-    // {
-    //     name: name,
-    //     track: 'Pueblo Motorsports Park - Pueblo, Co' ,
-    //     date: [5,14],
-    //     sortDate: .0514
-    // },
-    // {
-    //     name: name,
-    //     track: 'IMI Motorsports Park - Dacono, Co',
-    //     date: [6,4],
-    //     sortDate: .0604
-    // },
-    // {
-    //     name: name,
-    //     track: 'Colorado National Speedway - Dacono, Co',
-    //     date: [7,9],
-    //     sortDate: .0709
-    // },
-    // {
-    //     name: name,
-    //     track: 'IMI Motorsports Park - Dacono, Co',
-    //     date: [7,30],
-    //     sortDate: .0730
-    // },
-    // {
-    //     name: name,
-    //     track: 'Colorado National Speedway - Dacono, Co',
-    //     date: [8,20],
-    //     sortDate: .0813
-    // },
-    // {
-    //     name: name,
-    //     track: 'Grand Junction Motor Speedway - Grand Junction, Co',
-    //     date: [9,17],
-    //     sortDate: .0917
-    // }
+
+     {
+         name: name,
+         track: 'IMI Motorsports Park - Dacono, Co',
+         date: [5,25],
+         sortDate: .0525
+     },
+     {
+         name: name,
+         track: 'Colorado National Speedway - Dacono, Co',
+         date: [5,4],
+         sortDate: .0504
+     },
+     {
+         name: name,
+         track: 'TBD',
+         date: [6,8],
+         sortDate: .0608
+     },
+     {
+         name: name,
+         track: '(NASS Q) IMI Motorsports Park - Dacono, Co',
+         date: [7,13],
+         sortDate: .0713
+     },
+     {
+         name: name,
+         track: 'MI Motorsports Park - Dacono, Co',
+         date: [8,3],
+         sortDate: .0803
+     },
+     {
+        name: name,
+        track: 'Pueblo Motorsports Park - Pueblo, Co' ,
+        date: [9,7],
+        sortDate: .0907
+    },
 ]
 const exportRaces = {
     races, name
